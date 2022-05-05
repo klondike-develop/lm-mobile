82075d651cb9501ecb75963738e0bb5e9caaea73
Igor Melnikov <igor2354@yandex.ru>
2022-05-05 13:32:19 +0300

    2

--- js/new-swipe-catalog.js
+++ js/new-swipe-catalog.js
@@ -1,12 +1,22 @@
 $(document).ready(function() {
-   var sliderImageCard = new Swiper('.catalog-product-slider', {
-        spaceBetween: 10,
-        slidesPerView: 1,
-        watchOverflow: true,
 
-        pagination: {
-            el: '.catalog-product-slider .swiper-pagination',
-            type: 'bullets',
-        },
+   $.each($(".catalog-product-slider"), function(index, val) {
+        var activeLoop = false;
+
+        if ($(val).find(".swiper-slide").length > 1) {
+            activeLoop = true;
+        }
+
+        var sliderImageCard = new Swiper(val, {
+            spaceBetween: 10,
+            slidesPerView: 1,
+            watchOverflow: true,
+            loop: activeLoop,
+
+            pagination: {
+                el: '.catalog-product-slider .swiper-pagination',
+                type: 'bullets',
+            },
+        });
     });
 });
