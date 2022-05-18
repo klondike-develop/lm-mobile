e8fea3d48a2984ee92a1f10af3ea034b7beeed2f
Igor Melnikov <igor2354@yandex.ru>
2022-05-18 11:49:57 +0300

    21

--- js/new-product-collections.js
+++ js/new-product-collections.js
@@ -1,13 +1,12 @@
-$(document).ready(function() {
-    var newCollectionsSlider = new Swiper('.new-collections-slider', {
-        slidesPerView: "auto",
-        watchOverflow: true,
-        spaceBetween: 15,
+$(document).ready(function () {
+	var newCollectionsSlider = new Swiper(".new-collections-slider", {
+		slidesPerView: "auto",
+		watchOverflow: true,
+		spaceBetween: 15,
 
-
-        scrollbar: {
-            el: '.new-collections-slider .swiper-scrollbar',
-            draggable: true,
-        },
-    });
+		scrollbar: {
+			el: ".new-collections-slider .swiper-scrollbar",
+			draggable: true,
+		},
+	});
 });
