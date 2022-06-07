079eb4b6f47c7a42556e98700c539f6cfb7fb8d8
Igor Melnikov <igor2354@yandex.ru>
2022-06-07 10:16:56 +0300

    37

--- js/core.js
+++ js/core.js
@@ -601,35 +601,7 @@ $(document).ready(function () {
 
 	//------------------------------------------------------------------------//
 
-	//cart product delete
-	$(document).on("click", ".cart-product-delete", function (event) {
-		event.preventDefault();
 
-		var thisEl = $(this);
-
-		if ($(this).parents(".cart-product-complect").find(".cart-product-wrapper").length <= 1) {
-			thisEl.parents(".cart-product-wrapper").slideUp(300, function () {
-				thisEl.parents(".cart-product-wrapper").remove();
-			});
-		} else {
-			thisEl
-				.parents(".cart-product-wrapper")
-				.first()
-				.slideUp(300, function () {
-					thisEl.parents(".cart-product-wrapper").first().remove();
-				});
-		}
-	});
-
-	$(document).on("click", ".cart-product-back-close", function (event) {
-		event.preventDefault();
-
-		var thisEl = $(this);
-
-		thisEl.parents(".cart-product-wrapper").slideUp(300, function () {
-			thisEl.parents(".cart-product-wrapper").first().remove();
-		});
-	});
 
 	//------------------------------------------------------------------------//
 
--- js/new-cart.js
+++ js/new-cart.js
@@ -1,8 +1,52 @@
 $(document).ready(function () {
+	//cart product delete
+	$(document).on("click", ".cart-product-delete", function (event) {
+		event.preventDefault();
+
+		var thisEl = $(this);
+
+		if ($(this).parents(".cart-product-complect").find(".cart-product-wrapper").length <= 1) {
+			thisEl.parents(".cart-product-wrapper").slideUp(300, function () {
+				thisEl.parents(".cart-product-wrapper").remove();
+			});
+		} else {
+			thisEl
+				.parents(".cart-product-wrapper")
+				.first()
+				.slideUp(300, function () {
+					thisEl.parents(".cart-product-wrapper").first().remove();
+				});
+		}
+	});
+
+	$(document).on("click", ".cart-product-back-close", function (event) {
+		event.preventDefault();
+
+		var thisEl = $(this);
+
+		thisEl.parents(".cart-product-wrapper").slideUp(300, function () {
+			thisEl.parents(".cart-product-wrapper").first().remove();
+		});
+	});
+
 	$(".js-toggle-complect").on("click", function () {
 		var listComplect = $(this).parents(".cart-product-complect").find(".cart-product-complect-list");
 
 		$(this).toggleClass("show");
 		listComplect.slideToggle();
+	});
+
+	$(".cart-product-back-btn").on("click", function() {
+		var container = $(this).parents(".cart-product-wrapper");
+		var idProductDel = container.attr("data-deleted-id");
+		var elProductDel = $( "#" + idProductDel);
+
+		$(elProductDel).slideDown();
+
+		container.slideUp();
+
+		setTimeout(() => {	
+			container.remove();
+		}, 400);
 	});
 });
